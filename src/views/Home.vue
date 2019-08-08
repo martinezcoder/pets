00c772357a464444368ee5d73648e19998008b75
Fran Martinez <martinezcoder@gmail.com>
2019-08-08 13:19:18 +0200

    add pet form and action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="home">
     <h1>Adopt a new best friend.</h1>
-    <button class="btn btn-primary">Add new pet</button>
+    <button @click="togglePetForm" class="btn btn-primary">Add new pet</button>
 
-    <b-form @submit="handleSubmit" v-if="showPetForm">
+    <b-form @submit.prevent="handleSubmit" v-if="showPetForm">
       <b-form-group id="input-group-2" label="Pet's Name:" label-for="input-2">
         <b-form-input id="input-2" type="text" v-model="formData.name" required placeholder="Enter name"></b-form-input>
       </b-form-group>
@@ -16,17 +16,18 @@
         <b-form-input id="input-2" type="number" v-model="formData.age" required placeholder="Enter age"></b-form-input>
       </b-form-group>
 
-
       <b-button type="submit" variant="primary">Submit</b-button>
       <b-button type="reset" variant="danger">Reset</b-button>
     </b-form>
+
     <b-card class="mt-3" header="Form Data Result">
-      <pre class="m-0">{{ form }}</pre>
+      <pre class="m-0">{{ formData }}</pre>
     </b-card>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 
 export default {
   name: 'home',
@@ -40,13 +41,32 @@ export default {
       }
     }
   },
-
   methods: {
+    ...mapActions([
+      'addPet'
+    ]),
     togglePetForm () {
       this.showPetForm = !this.showPetForm
     },
     handleSubmit () {
-      //
+      const { species, name, age } = this.formData
+      const payload = {
+        species,
+        pet: {
+          name,
+          age
+        }
+      }
+      this.addPet(payload)
+
+      // reset the form after submit
+
+      this.formData = {
+        name: '',
+        age: 0,
+        species: null
+      }
+      this.showPetForm = false
     }
   }
 }
